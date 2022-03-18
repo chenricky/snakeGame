c93c45be8767c0a97f17c0d302364d3305156055
ricky <chenricky@gmail.com>
2022-03-18 16:34:43 -0700

    add colours and print location

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+snake_colours=["white", "green", "blue"]
 x_start_position = [0,20,40]
 y_start_position = []
 
 for i in range(0,3):
     turtle = Turtle(shape="square")
-    turtle.color("white")
+    turtle.color(snake_colours[i])
     turtle.penup()
     turtle.goto(x=x_start_position[i], y=0)
+    print(turtle.position()[0])
 
 
 
