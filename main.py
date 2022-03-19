f559878b48116943acd2167aa4b9813a64376486
ricky <chenricky@gmail.com>
2022-03-18 22:10:07 -0700

    add onkey

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ screen.title("My Snake Game")
 snake_colours=["white", "green", "blue"]
 x_start_position = [0,20,40]
 y_start_position = []
+turtle_list=[]
 
 for i in range(0,3):
     turtle = Turtle(shape="square")
@@ -14,22 +15,21 @@ for i in range(0,3):
     turtle.penup()
     turtle.goto(x=x_start_position[i], y=0)
     print(turtle.position()[0])
+    turtle_list.append(turtle)
+
+print(turtle_list)
 
 
+def forward():
+    for j in range(3):
+        turtle_list[j].pendown()
+        turtle_list[j].forward(50)
 
-
-
-
-
-
-
-
-
-
-
-
-
+screen.update()
+screen.onkey(forward(),"Up")
+screen.listen()
 
 
 
 screen.exitonclick()
+screen.mainloop()
