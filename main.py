77481143d5c6379598ca931902b0705d71588abb
ricky <chenricky@gmail.com>
2022-03-19 15:27:46 -0700

    fulling working left and right with static code

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-snake_colours=["white", "green", "blue"]
+snake_colours=["red", "green", "blue"]
 x_start_position = [0,20,40]
 y_start_position = []
 turtle_list=[]
 
 for i in range(0,3):
-    turtle = Turtle(shape="arrow")
+    turtle = Turtle(shape="square")
+    turtle.shapesize(1,1,1)
     turtle.color(snake_colours[i])
+    turtle.write(i)
     turtle.penup()
     turtle.goto(x=x_start_position[i], y=0)
     print(turtle.position()[0])
@@ -31,16 +33,22 @@ def backward():
         turtle_list[j].backward(50)
 
 def left():
-    for j in range(3):
-        # turtle_list[j].pendown()
-        turtle_list[j].left(90)
-        # turtle_list[j].forward(50)
+    turtle_list[2].left(90)
+    turtle_list[2].forward(40)
+    turtle_list[1].forward(20)
+    turtle_list[1].left(90)
+    turtle_list[1].forward(20)
+    turtle_list[0].forward(40)
+    turtle_list[0].left(90)
 
 def right():
-    for j in range(3):
-        # turtle_list[j].pendown()
-        turtle_list[j].right(90)
-        # turtle_list[j].forward(50)
+    turtle_list[2].right(90)
+    turtle_list[2].forward(40)
+    turtle_list[1].forward(20)
+    turtle_list[1].right(90)
+    turtle_list[1].forward(20)
+    turtle_list[0].forward(40)
+    turtle_list[0].right(90)
 
 
 screen.update()
