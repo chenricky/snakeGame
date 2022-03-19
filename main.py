0141cf4c30f758d3cb97b9d902e8f79c4bf454ea
ricky <chenricky@gmail.com>
2022-03-18 22:17:59 -0700

    added soem control. not entirely working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ y_start_position = []
 turtle_list=[]
 
 for i in range(0,3):
-    turtle = Turtle(shape="square")
+    turtle = Turtle(shape="arrow")
     turtle.color(snake_colours[i])
     turtle.penup()
     turtle.goto(x=x_start_position[i], y=0)
@@ -22,11 +22,33 @@ print(turtle_list)
 
 def forward():
     for j in range(3):
-        turtle_list[j].pendown()
+        # turtle_list[j].pendown()
         turtle_list[j].forward(50)
 
+def backward():
+    for j in range(3):
+        # turtle_list[j].pendown()
+        turtle_list[j].backward(50)
+
+def left():
+    for j in range(3):
+        # turtle_list[j].pendown()
+        turtle_list[j].left(90)
+        # turtle_list[j].forward(50)
+
+def right():
+    for j in range(3):
+        # turtle_list[j].pendown()
+        turtle_list[j].right(90)
+        # turtle_list[j].forward(50)
+
+
 screen.update()
-screen.onkey(forward,"Up")
+screen.onkey(forward,"w")
+screen.onkey(backward,"s")
+screen.onkey(left,"a")
+screen.onkey(right,"d")
+
 screen.listen()
 
 
